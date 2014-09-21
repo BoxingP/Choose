ff958b948cd28c97d3a6c0383907a60bcb1d2810
Boxing Peng <bxpeng@thoughtworks.com>
2014-09-22 02:05:35 +0800

    H2: add test in HotelBookingTest. Boxing

--- src/main/java/com/boxing/unit/Hotelbooking.java
+++ src/main/java/com/boxing/unit/Hotelbooking.java
@@ -4,4 +4,8 @@ public class HotelBooking {
     public int getWeekdayAmount(String date) {
         return 1;
     }
+
+    public int getWeekendAmount(String date) {
+        return 1;
+    }
 }
--- src/test/java/com/boxing/unit/HotelBookingTest.java
+++ src/test/java/com/boxing/unit/HotelBookingTest.java
@@ -7,8 +7,14 @@ import static org.hamcrest.core.Is.is;
 
 public class HotelBookingTest {
     @Test
-    public void whenDateIsMonWeekdayAmount_shouldReturn_One() {
+    public void dateIsMonWeekdayAmount_shouldReturn_One() {
         HotelBooking hotelBooking = new HotelBooking();
         assertThat(hotelBooking.getWeekdayAmount("22Sep2014(mon)"), is(1));
+    }
+
+    @Test
+    public void dateIsSatWeekendAmount_shouldReturn_One() {
+        HotelBooking hotelBooking = new HotelBooking();
+        assertThat(hotelBooking.getWeekendAmount("27Sep2014(sat)"), is(1));
     }
 }
