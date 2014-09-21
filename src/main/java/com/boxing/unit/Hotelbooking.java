7c8016de0c3a53a10dd86c19dd4d450c5612533f
Boxing Peng <bxpeng@thoughtworks.com>
2014-09-22 07:55:52 +0800

    H2: implement the methods of get weekday and weekend amounts. Boxing

--- src/main/java/com/boxing/unit/Hotelbooking.java
+++ src/main/java/com/boxing/unit/Hotelbooking.java
@@ -1,11 +1,22 @@
 package com.boxing.unit;
 
 public class HotelBooking {
+    private String[] weekday = new String[]{"mon", "tues", "wed", "thur", "fri"};
+    private String[] weekend = new String[]{"sat", "sun"};
+
     public int getWeekdayAmount(String date) {
-        return 1;
+        for (int i = 0; i < weekday.length; i++) {
+            if (date.contains(weekday[i]))
+                return 1;
+        }
+        return 0;
     }
 
     public int getWeekendAmount(String date) {
-        return 1;
+        for (int i = 0; i < weekend.length; i++) {
+            if (date.contains(weekend[i]))
+                return 1;
+        }
+        return 0;
     }
 }
