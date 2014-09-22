02009c7b8f2af1ec7b891a64eda5742dc89fde79
Boxing Peng <bxpeng@thoughtworks.com>
2014-09-22 12:34:41 +0800

    H2: add a test in HotelSystemTest. Boxing

--- src/test/java/com/boxing/hotelSystem/HotelSystemTest.java
+++ src/test/java/com/boxing/hotelSystem/HotelSystemTest.java
@@ -14,10 +14,16 @@ public class HotelSystemTest {
     }
 
     @Test
-    public void aRegularWithFriSatSunBookInformation_shouldReturn_lakewood() {
+    public void aRegularWithFriSatSunBookInformation_shouldReturn_bridgewood() {
         String bookInformation = "Regular: 26Sep2014(fri), 27Sep2014(sat), 28Sep2014(sun)";
         HotelSystem hotelSystem = new HotelSystem();
         assertThat(hotelSystem.process(bookInformation), is("Bridgewood"));
     }
 
+    @Test
+    public void aRewardsWithThurFriSatBookInformation_shouldReturn_ridgewood() {
+        String bookInformation = "Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)";
+        HotelSystem hotelSystem = new HotelSystem();
+        assertThat(hotelSystem.process(bookInformation), is("Ridgewood"));
+    }
 }
