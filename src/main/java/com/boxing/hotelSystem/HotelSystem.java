098299a0e9b10a63fdbe2f918702350dd8f61288
Boxing Peng <bxpeng@thoughtworks.com>
2014-09-22 16:42:09 +0800

    H2: change the implement of HotelSystem. Boxing

--- src/main/java/com/boxing/hotelSystem/HotelSystem.java
+++ src/main/java/com/boxing/hotelSystem/HotelSystem.java
@@ -2,14 +2,17 @@ package com.boxing.hotelSystem;
 
 import com.boxing.unit.HotelBooking;
 import com.boxing.unit.HotelChooser;
+import com.boxing.unit.PrintOutput;
 
 public class HotelSystem {
-    public String process(String bookInformation) {
+    public static void main(String[] args) {
+        String bookInformation = args[0];
         HotelBooking booking = new HotelBooking(bookInformation);
         String clientClass = booking.getClientClass();
         int weekdayAmount = booking.getWeekdayAmount();
         int weekendAmount = booking.getWeekendAmount();
         HotelChooser chooser = new HotelChooser();
-        return chooser.choose(clientClass, weekdayAmount,weekendAmount);
+        String result = chooser.choose(clientClass, weekdayAmount,weekendAmount);
+        new PrintOutput().print(result);
     }
 }
--- src/test/java/com/boxing/hotelSystem/HotelSystemTest.java
+++ src/test/java/com/boxing/hotelSystem/HotelSystemTest.java
@@ -1,29 +1,52 @@
 package com.boxing.hotelSystem;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class HotelSystemTest {
+    private HotelSystem hotelSystem;
+    private ByteArrayOutputStream outputStream;
+
+    @Before
+    public void initObject() {
+        hotelSystem = new HotelSystem();
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
     @Test
     public void aRegularWithMonTuesWedBookInformation_shouldReturn_lakewood() {
         String bookInformation = "Regular: 22Sep2014(mon), 23Sep2014(tues), 24Sep2014(wed)";
-        HotelSystem hotelSystem = new HotelSystem();
-        assertThat(hotelSystem.process(bookInformation), is("Lakewood"));
+        String[] args=new String[] {bookInformation};
+        hotelSystem.main(args);
+        String output = this.outputStream.toString();
+        String separator = System.getProperty("line.separator");
+        assertThat(output, is("Lakewood"+separator));
     }
 
     @Test
     public void aRegularWithFriSatSunBookInformation_shouldReturn_bridgewood() {
         String bookInformation = "Regular: 26Sep2014(fri), 27Sep2014(sat), 28Sep2014(sun)";
-        HotelSystem hotelSystem = new HotelSystem();
-        assertThat(hotelSystem.process(bookInformation), is("Bridgewood"));
+        String[] args=new String[] {bookInformation};
+        hotelSystem.main(args);
+        String output = this.outputStream.toString();
+        String separator = System.getProperty("line.separator");
+        assertThat(output, is("Bridgewood"+separator));
     }
 
     @Test
     public void aRewardsWithThurFriSatBookInformation_shouldReturn_ridgewood() {
         String bookInformation = "Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)";
-        HotelSystem hotelSystem = new HotelSystem();
-        assertThat(hotelSystem.process(bookInformation), is("Ridgewood"));
+        String[] args=new String[] {bookInformation};
+        hotelSystem.main(args);
+        String output = this.outputStream.toString();
+        String separator = System.getProperty("line.separator");
+        assertThat(output, is("Ridgewood"+separator));
     }
 }
