e4758c6fd1fb0d691dbeadcf6fd45119da14d342
Boxing Peng <bxpeng@thoughtworks.com>
2014-09-26 09:09:35 +0800

    H2: delete repeating in SystemTest. Boxing

--- src/test/java/com/boxing/hotelSystem/HotelSystemTest.java
+++ src/test/java/com/boxing/hotelSystem/HotelSystemTest.java
@@ -12,41 +12,48 @@ import static org.hamcrest.core.Is.is;
 public class HotelSystemTest {
     private HotelSystem hotelSystem;
     private ByteArrayOutputStream outputStream;
+    private String[] args;
+    private String separator;
 
     @Before
     public void initObject() {
         hotelSystem = new HotelSystem();
         outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
+        args=new String[1];
+        separator = System.getProperty("line.separator");
     }
 
     @Test
     public void aRegularWithMonTuesWedBookInformation_shouldReturn_lakewood() {
         String bookInformation = "Regular: 22Sep2014(mon), 23Sep2014(tues), 24Sep2014(wed)";
-        String[] args = new String[]{bookInformation};
+        args[0] = bookInformation;
         hotelSystem.main(args);
         String output = this.outputStream.toString();
-        String separator = System.getProperty("line.separator");
-        assertThat(output, is("Lakewood" + separator));
+        String expectHotelName = "Lakewood" + separator;
+
+        assertThat(output, is(expectHotelName));
     }
 
     @Test
     public void aRegularWithFriSatSunBookInformation_shouldReturn_bridgewood() {
         String bookInformation = "Regular: 26Sep2014(fri), 27Sep2014(sat), 28Sep2014(sun)";
-        String[] args = new String[]{bookInformation};
+        args[0] = bookInformation;
         hotelSystem.main(args);
         String output = this.outputStream.toString();
-        String separator = System.getProperty("line.separator");
-        assertThat(output, is("Bridgewood" + separator));
+        String expectHotelName = "Bridgewood" + separator;
+
+        assertThat(output, is(expectHotelName));
     }
 
     @Test
     public void aRewardsWithThurFriSatBookInformation_shouldReturn_ridgewood() {
         String bookInformation = "Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)";
-        String[] args = new String[]{bookInformation};
+        args[0] = bookInformation;
         hotelSystem.main(args);
         String output = this.outputStream.toString();
-        String separator = System.getProperty("line.separator");
-        assertThat(output, is("Ridgewood" + separator));
+        String expectHotelName = "Ridgewood" + separator;
+
+        assertThat(output, is(expectHotelName));
     }
 }
