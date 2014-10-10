2168ef806fc6a23ae5a958d73995526882423646
Boxing <boxingp@gmail.com>
2014-10-10 18:02:12 +0800

    add result servlet.

--- src/main/java/com/boxing/hotel/servlet/ResultServlet.java
+++ src/main/java/com/boxing/hotel/servlet/ResultServlet.java
@@ -1,6 +1,21 @@
 package com.boxing.hotel.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class ResultServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream outputStream = resp.getOutputStream();
+        resp.setContentType("text/html");
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+        outputStreamWriter.write("<div>The recommend hotel is Lakewood.</div>");
+        outputStreamWriter.write("<a href=\"index\">Back</a>");
+        outputStreamWriter.flush();
+    }
 }
